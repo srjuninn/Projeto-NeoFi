f0e9e5a1193df2cb7b460af942915e568d9d3cbb
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-08-05 17:00:37 -0300

    feat: add promise to wait and hidden hamburger menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -7,9 +7,23 @@ let fechar = document.getElementById("fecharHamburger");
 abrir.addEventListener("click", () => {
   hamburger.style.display = "none";
   shape.style.display = "block";
-  shape.style.left = "0";
+  new Promise((chamar) => {
+    setTimeout(() => {
+      chamar();
+    }, 200);
+  }).then(() => {
+    shape.style.left = "-15px";
+  });
+  // shape.style.position = "fixed";
 });
 fechar.addEventListener("click", () => {
   shape.style.left = "-300px";
   hamburger.style.display = "flex";
+  new Promise((tirar) => {
+    setTimeout(() => {
+      tirar();
+    }, 1000);
+  }).then(() => {
+    shape.style.display = "none";
+  });
 });
