2e36516c715632a8c9fd20f682d8855651f3915b
Ryan Silva <ryanferreiradasilva2006@gmail.com>
2025-08-05 17:56:00 -0300

    feat: hide header and hamburger menu

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,12 +1,20 @@
 // menu hamburgão
+let ocultar = document.getElementById("header")
+let headerLogo = document.getElementById("headerLogo")
+let textLogo = document.getElementById("textLogo")
+let buttonsHeader = document.getElementById("buttonsHeader")
 let abrir = document.getElementById("abrirHamburger");
 let hamburger = document.getElementById("hamburger");
 let shape = document.getElementById("menuShape");
 let fechar = document.getElementById("fecharHamburger");
 
 abrir.addEventListener("click", () => {
+  headerLogo.style.display = "none";
+  textLogo.style.display = "none";
+  buttonsHeader.style.display = "none";
   hamburger.style.display = "none";
   shape.style.display = "block";
+  document.body.classList.add("bodylockScroll");
   new Promise((chamar) => {
     setTimeout(() => {
       chamar();
@@ -17,8 +25,12 @@ abrir.addEventListener("click", () => {
   // shape.style.position = "fixed";
 });
 fechar.addEventListener("click", () => {
+  headerLogo.style.display = "flex";
+  textLogo.style.display = "flex";
+  buttonsHeader.style.display = "flex";
   shape.style.left = "-300px";
   hamburger.style.display = "flex";
+  document.body.classList.remove("bodylockScroll");
   new Promise((tirar) => {
     setTimeout(() => {
       tirar();
